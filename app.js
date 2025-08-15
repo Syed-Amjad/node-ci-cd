a8d08779dde389c506eb81fd5574df7d475ab2cb
Syed Amjad Ali <143636893+Syed-Amjad@users.noreply.github.com>
2025-08-15 21:12:08 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello from Node.js CI/CD with Jenkins!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-module.exports = app; // for testing
-
+module.exports = app; // only export app for tests
